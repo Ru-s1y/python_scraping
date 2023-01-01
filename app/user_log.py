2aab9375980c5a4ed24605c4aec26908488c708c
Rus1y <sakura.rus1y@gmail.com>
2023-01-02 04:22:08 +0900

    [update] selenium base

--- app/user_log.py
+++ app/user_log.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 class user_log:
 
-    def save(response):
+    def save_html(response):
         now = datetime.now()
         path = './logs/' + now.strftime("%Y_%m_%d_%H_%M_%S") + '.html'
 
